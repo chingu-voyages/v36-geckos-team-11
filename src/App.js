7b288d666a9dc0a4de62edb67f5dac09c0e965b1
Emmanuel Okon <71082695+EmmanuelOkon@users.noreply.github.com>
2022-01-15 17:17:00 +0100

    created react app, installed bootstrap and font awesome using terminal

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
-import React from 'react'
+import React from 'react';
+import './index.css';
+// import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row'
+import Navbar from './Navbar.jsx'
+
 
 const App = () => {
   return (
-    <div>
-      Hello
-    </div>
-  )
-}
+    <div className='lg' >
+      <Row >
+        <Navbar />
 
-export default App
+      </Row>
+    </div>
+  );
+};
+
+export default App;
+
+// className="justify-content-md-center"
