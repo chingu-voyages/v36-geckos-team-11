e9162b52eccbd320996d46da832f77fc76adecf8
Avic Ndugu <37213663+avicndugu@users.noreply.github.com>
2022-02-11 16:44:28 +0300

    Display the error on the page

--- src/RestaurantCardsContainer.jsx
+++ src/RestaurantCardsContainer.jsx
@@ -9,11 +9,12 @@ class RestaurantCardsContainer extends React.Component {
     super(props);
     this.state = {
       restaurants: [],
+      error: null,
     };
   }
 
   componentDidMount() {
-    fetch('https://learnreact.avicndugu.repl.co/restaurants.json')
+    fetch('https://learnreact.avicndugu.repl.co/resstaurants.json')
       .then((response) => {
         if (response.ok) {
           return response.json();
@@ -21,35 +22,43 @@ class RestaurantCardsContainer extends React.Component {
           throw Error(response.status);
         }
       })
-      .then((data) =>
-        this.setState({
-          restaurants: data.restaurants,
-        })
-      )
-      .catch((error) => {
-        // Handle the error
-        console.log(error);
-      });
+      .then(
+        (data) => {
+          this.setState({
+            restaurants: data.restaurants,
+          });
+        },
+        (error) => {
+          this.setState({
+            error: error,
+          });
+        }
+      );
   }
 
   render() {
-    const { restaurants } = this.state;
-    return (
-      <Container>
-        <Row xs={1} md={3}>
-          {restaurants.map((restaurant, index) => (
-            <Col>
-              <RestaurantCard
-                key={index}
-                name={restaurant.restaurantName}
-                address={restaurant.address}
-                hours={restaurant.hours}
-              />
-            </Col>
-          ))}
-        </Row>
-      </Container>
-    );
+    const { restaurants, error } = this.state;
+    if (error) {
+      return <div>Error code/ message: {error.message}</div>;
+    } else {
+      return (
+        <Container>
+          <Row xs={1} md={3}>
+            {restaurants.map((restaurant, index) => (
+              <Col>
+                <p>{error}</p>
+                <RestaurantCard
+                  key={index}
+                  name={restaurant.restaurantName}
+                  address={restaurant.address}
+                  hours={restaurant.hours}
+                />
+              </Col>
+            ))}
+          </Row>
+        </Container>
+      );
+    }
   }
 }
 
